565fe5e17a1baabacb6b0f0143b836e19f7f208c
Muhammed Enes AYDIN <aydinenes521@gmail.com>
2024-12-05 20:28:07 +0300

    Add files via upload

---
+++ 01 c# eğitim/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_c__eğitim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region YazdırmaKomutları
+            //Console.WriteLine("hello world");
+            //Console.Write("selam");
+            //Console.WriteLine("+++++ oyun kategorileri +++++");
+            //Console.WriteLine();
+            //Console.WriteLine("1-Aksiyon-macera");
+            //Console.WriteLine("2-souls-like");
+            //Console.WriteLine("3-RPG");
+            //Console.WriteLine("4-platform");
+            //Console.WriteLine("5-mmo");
+            //Console.WriteLine("6-real time strategy");
+            //Console.WriteLine();
+            //Console.WriteLine("+++++ oyun kategorileri +++++");
+
+            #endregion
+
+            #region StringDeğişkenler
+
+            //string name;
+            //name = "memes";
+            //Console.Write(name);
+
+
+            //string customerName;
+            //string customerSurname;
+            //    string customerPhone;
+            //    string customerEmail, district, city;
+            //customerName = "enes";
+            //customerSurname = "aydın";
+            //customerPhone = "+90 552 582 31 46";
+            //customerEmail = "aydinenes521@gmail.com";
+            //district = "afşin";
+            //city = "kahramanmaraş";
+            //Console.WriteLine();
+            //Console.WriteLine("++++ Rezervasyon Kartı ++++");
+            //Console.WriteLine("--------------------------------------");
+            //Console.WriteLine();
+            //Console.WriteLine("müşteri:" + customerName + " " + customerSurname);
+            //Console.WriteLine("iletişim:" + customerPhone);
+            //Console.WriteLine("Email adresi:" + customerEmail);
+            //Console.WriteLine("Adres:" + district + "/" + city);
+            //Console.WriteLine("--------------------------------------");
+            //Console.WriteLine();
+            //customerName = "erkan";
+            //customerSurname = "ahmet";
+            //customerPhone = "90+ 500 300 40 50";
+            //customerEmail = "erkaaydingts@gmail.com";
+            //district = "yeşilyurt";
+            //city = "malatya";
+            //Console.WriteLine("--------------------------------------");
+
+            //Console.WriteLine();
+            //Console.WriteLine("müşteri:" + customerName + " " + customerSurname);
+            //Console.WriteLine("iletişim:" + customerPhone);
+            //Console.WriteLine("Email adresi:" + customerEmail);
+            //Console.WriteLine("Adres:" + district + "/" + city);
+
+            //Console.WriteLine("--------------------------------------");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #endregion
+
+
+            #region IntDeğişkenler
+
+            //int number = 31;
+            //Console.Write(number);
+
+            //int RedDeadRedemptionPrice = 60;
+            //int calloffDutyPrice = 70;
+            //int subnaticaPrice = 15;
+            //int stardewValleyPrice = 10;
+
+
+            //Console.WriteLine("--------------Steam Fiyat Listesi---------------");
+            //Console.WriteLine();
+            //Console.WriteLine("-----Red Dead Redemption:" + RedDeadRedemptionPrice + "$");
+            //Console.WriteLine("-----call off Duty:" + calloffDutyPrice + "$");
+            //Console.WriteLine("-----subnatica:" + subnaticaPrice + "$");
+            //Console.WriteLine("-----stardew Valley:" + stardewValleyPrice + "$");
+
+            //Console.WriteLine("--------------Steam Fiyat Listesi---------------");
+
+            //int redDeadRedemptioncount;
+            //int calloffDutycount;
+            //int subnaticacount;
+            //int stardewValleycount;
+
+            //int totalredDeadRedemptionPrice;
+            //int totalcalloffDutyPrice;
+            //int totalsubnaticaPrice;
+            //int totalstardewValleyPrice;
+            //int totalprice;
+
+            //redDeadRedemptioncount = 5;
+            //calloffDutycount = 10;
+            //subnaticacount = 2;
+            //stardewValleycount = 5;
+
+            //totalredDeadRedemptionPrice = redDeadRedemptioncount * RedDeadRedemptionPrice;
+            //totalcalloffDutyPrice = calloffDutycount * calloffDutyPrice;
+            //totalsubnaticaPrice = subnaticacount * subnaticaPrice;
+            //totalstardewValleyPrice = stardewValleycount * stardewValleyPrice;
+            //totalprice = totalstardewValleyPrice + totalredDeadRedemptionPrice + totalsubnaticaPrice + totalcalloffDutyPrice;
+
+            //Console.WriteLine("-------------------------------");
+            //Console.WriteLine( "RDR tutarı: " +  totalredDeadRedemptionPrice + "$");
+            //Console.WriteLine( "call of duty tutarı:" +totalcalloffDutyPrice + "$");
+            //Console.WriteLine("subnatica tutarı:" + totalsubnaticaPrice + "$");
+            //Console.WriteLine("stardew Valley tutarı:" + totalstardewValleyPrice + "$");
+            //Console.WriteLine("toplam tutar:" + totalprice + "$");
+
+
+
+
+
+
+
+
+            #endregion
+
+
+
+            Console.Read();
+        }
+    }
+}
